7a79557b4c191299a7414727c2bd303d749a5902
Chris Riebschlager <criebschlager@dimin.com>
2018-10-27 01:11:50 -0500

    nice

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,12 +4,17 @@ void ofApp::setup()
 {
     shuffleSlices.addListener(this, &ofApp::onShuffleSlicesPressed);
     saveImage.addListener(this, &ofApp::onSaveImagePressed);
+    clear.addListener(this, &ofApp::onClearPressed);
 
     gui.setup();
     gui.add(minAlpha.setup("minAlpha", 0, 0, 255));
     gui.add(maxAlpha.setup("maxAlpha", 255, 0, 255));
+    gui.add(randomAlphaFrequency.setup("randomAlphaFrequency", 0, 0, 1));
     gui.add(minRotation.setup("minRotation", -180, -360, 360));
     gui.add(maxRotation.setup("maxRotation", 180, -360, 360));
+    gui.add(randomRotationFrequency.setup("randomRotationFrequency", 0, 0, 1));
+    gui.add(lowPass.setup("lowPass", 0, 0, 1));
+    gui.add(highPass.setup("highPass", 1, 0, 1));
     gui.add(minScale.setup("minScale", -1, -5, 5));
     gui.add(maxScale.setup("maxScale", 1, -5, 5));
     gui.add(jitterScaleAmount.setup("jitterScaleAmount", 0, 0, 10));
@@ -20,8 +25,10 @@ void ofApp::setup()
     gui.add(timeSkipFrequency.setup("timeSkipFrequency", 0, 0, 0.1));
     gui.add(rendersPerFrame.setup("rendersPerFrame", 1000, 1, 5000));
     gui.add(randomSliceFrequency.setup("randomSliceFrequency", 0, 0, 1));
+    gui.add(sourceChangeFrequency.setup("sourceChangeFrequency", 0, 0, 1));
     gui.add(shuffleSlices.setup("shuffleSlices"));
     gui.add(saveImage.setup("saveImage"));
+    gui.add(clear.setup("clear"));
 
     canvas.allocate(1920 * 2, 1080 * 2, GL_RGBA);
     canvas.begin();
@@ -29,7 +36,11 @@ void ofApp::setup()
     canvas.end();
 
     time = 0;
+    currentSourceIndex = 0;
+    isDrawing = false;
+
     loadSlices();
+    loadSources();
 }
 
 void ofApp::loadSlices()
@@ -48,12 +59,28 @@ void ofApp::loadSlices()
     gui.add(maxSliceIndex.setup("maxSliceIndex", slices.size(), 0, slices.size()));
 }
 
-void ofApp::render()
+void ofApp::loadSources()
 {
-    randomPoint.x = ofRandom(canvas.getWidth());
-    randomPoint.y = ofRandom(canvas.getHeight());
+    string path = "./sources/face-bright";
+    ofDirectory dir(path);
+    dir.allowExt("png");
+    dir.allowExt("jpg");
+    dir.listDir();
+    for (int i = 0; i < dir.size(); i++)
+    {
+        ofImage img;
+        img.load(dir.getPath(i));
+        img.setImageType(OF_IMAGE_GRAYSCALE);
+        img.resize(canvas.getWidth(), canvas.getHeight());
+        sources.push_back(img);
+    }
+}
 
-    float noise = ofNoise(randomPoint.x * noiseScale, randomPoint.y * noiseScale, time * noiseScale, time * noiseScale);
+void ofApp::render(int x, int y)
+{
+    ofColor color = sources[currentSourceIndex].getColor(x, y);
+
+    float noise = ofNoise(x * noiseScale, y * noiseScale, time * noiseScale);
     float scale = ofMap(noise, 0, 1, minScale, maxScale);
 
     if (ofRandom(1) < jitterScaleFrequency)
@@ -62,6 +89,12 @@ void ofApp::render()
     }
 
     float rotation = ofMap(noise, 0, 1, minRotation, maxRotation);
+
+    if (ofRandom(1) < randomRotationFrequency)
+    {
+        rotation = ofRandom(-360, 360);
+    }
+
     int sliceIndex = floor(ofMap(noise, 0, 1, minSliceIndex, maxSliceIndex));
 
     if (ofRandom(1) < randomSliceFrequency)
@@ -71,19 +104,24 @@ void ofApp::render()
 
     float alpha = ofMap(noise, 0, 1, minAlpha, maxAlpha);
 
+    if (ofRandom(1) < randomAlphaFrequency)
+    {
+        alpha = ofRandom(255);
+    }
+
+    if (noise < lowPass || noise > highPass)
+    {
+        return;
+    }
+
     canvas.begin();
     glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
     ofPushMatrix();
-    ofTranslate(randomPoint.x, randomPoint.y);
+    ofTranslate(x, y);
     ofScale(scale, scale);
     ofRotateDeg(rotation);
-    ofSetColor(alpha);
-
-    if (noise > 0.5)
-    {
-        slices[sliceIndex].draw(0, 0);
-    }
-
+    ofSetColor(color.getBrightness(), alpha);
+    slices[sliceIndex].draw(0, 0);
     ofPopMatrix();
     canvas.end();
 }
@@ -97,9 +135,19 @@ void ofApp::update()
         time += timeSkipAmount;
     }
 
+    if (ofRandom(1) < sourceChangeFrequency)
+    {
+        currentSourceIndex = floor(ofRandom(sources.size()));
+    }
+
     for (int i = 0; i < rendersPerFrame; i++)
     {
-        render();
+        if (isDrawing)
+        {
+            randomPoint.x = ofRandom(0, canvas.getWidth());
+            randomPoint.y = ofRandom(0, canvas.getHeight());
+            render(randomPoint.x, randomPoint.y);
+        }
     }
 }
 
@@ -120,6 +168,13 @@ void ofApp::onSaveImagePressed()
     saveFbo();
 }
 
+void ofApp::onClearPressed()
+{
+    canvas.begin();
+    ofClear(0, 0, 0);
+    canvas.end();
+}
+
 void ofApp::saveFbo()
 {
     ofFbo img;
@@ -136,11 +191,20 @@ void ofApp::saveFbo()
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key)
 {
+    if (key == ' ')
+    {
+        isDrawing = !isDrawing;
+    }
+    if (key == 's')
+    {
+        isMouseDrawing = true;
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key)
 {
+    isMouseDrawing = false;
 }
 
 //--------------------------------------------------------------
@@ -151,6 +215,13 @@ void ofApp::mouseMoved(int x, int y)
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button)
 {
+    if (isMouseDrawing)
+    {
+        int nx = floor(ofMap(x, 0, ofGetWidth(), 0, canvas.getWidth()));
+        int ny = floor(ofMap(y, 0, ofGetHeight(), 0, canvas.getHeight()));
+        ofLogNotice(ofToString(x), ofToString(y));
+        render(nx, ny);
+    }
 }
 
 //--------------------------------------------------------------
